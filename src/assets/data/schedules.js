ebea6cbfc00fca024d6e030a93933abde8b092b6
정수진 <sssjsjj@ipartners.co.kr>
2021-10-27 17:59:30 +0900

    sass설치, vue config셋팅

---
+++ src/assets/data/schedules.js
@@ -0,0 +1,65 @@
+[
+  {
+  "year": 2021,
+  "month": 2,
+  "date": [18],
+  "title": "약속",
+  "description": "#대기중"
+  },
+  {
+    "year": 2021,
+    "month": 1,
+    "date": [17],
+    "title": "도시락",
+    "description": "#하귀찮"
+  },
+  {
+    "year": 2021,
+    "month": 4,
+    "date": [8, 10],
+    "title": "가족들 만나는 날",
+    "description": "#보고싶다"
+  },
+  {
+    "year": 2017,
+    "month": 2,
+    "date": [27],
+    "title": "아이파 입사",
+    "description": "#월급만꼬박꼬박"
+  },
+  {
+    "year": 2018,
+    "month": 10,
+    "date": [6],
+    "title": "승진한 날",
+    "description": "#미스코리아된줄"
+  },
+  {
+    "year": 2019,
+    "month": 11,
+    "date": [3],
+    "title": "베스트 받은날",
+    "description": "#천군만마"
+  },
+  {
+    "year": 2021,
+    "month": 8,
+    "date": [9],
+    "title": "출근",
+    "description": ""
+  },
+  {
+    "year": 2021,
+    "month": 8,
+    "date": [12,15],
+    "title": "함평",
+    "description": "#누나형아언제와?"
+  },
+  {
+    "year": 2021,
+    "month": 10,
+    "date": [25],
+    "title": "월급날",
+    "description": "#급해요빨리요어서와"
+  }
+]
